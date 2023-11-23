406045780125501178b9c01a5359d2df73aa2c4b
polinadrrrr <99727608+polinadrrrr@users.noreply.github.com>
2023-11-23 14:22:59 +0700

    Update models.py

--- blog/main/models.py
+++ blog/main/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     name = models.CharField(max_length=50)
+    
 class Article(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
